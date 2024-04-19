44cc0cde1284fa4c4ac89bd0ca320732f4d8572d
joshua-frey-wsu <joshua.frey@wsu.edu>
2024-04-18 22:31:07 -0700

    Fixed db schema

--- database/forum.sql
+++ database/forum.sql
@@ -9,22 +9,21 @@
 
     CREATE TABLE IF NOT EXISTS DISCUSSION_TOPICS (
         topicID INT PRIMARY KEY AUTOINCREMENT NOT NULL, 
-        email TEXT NOT NULL, 
+        email TEXT NOT NULL, -- grab the email which is the unique primary key in users table to display username on posts, comments, etc.
         topicName TEXT NOT NULL, 
         topicDesc TEXT NOT NULL, 
         created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,  
         FOREIGN KEY (email) REFERENCES USERS(email)
     );
-
-    
+ 
     CREATE TABLE IF NOT EXISTS POSTS (
         postID INT PRIMARY KEY AUTOINCREMENT NOT NULL,
-        email TEXT NOT NULL,
+        email TEXT NOT NULL,  
         topicID INT NOT NULL,
         title TEXT NOT NULL,
         content TEXT NOT NULL,
         created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-        votes INT DEFAULT 0,
+        votes INT DEFAULT 0 CHECK(votes >= 0), -- default votes to 0, makes sure it can't be less than 0, then decrement or increment based on upvote or downvote
         FOREIGN KEY (email) REFERENCES USERS(email),
         FOREIGN KEY (topicID) REFERENCES DISCUSSION_TOPICS(topicID)
     );
